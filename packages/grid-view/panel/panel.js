58296f476b9edd3eac27d629923b523e67b9c7e6
Johnny Wu <KarasAya@gmail.com>
2015-05-06 03:20:24 +0800

    add panning

--- packages/grid-view/panel/panel.js
+++ packages/grid-view/panel/panel.js
@@ -5,6 +5,16 @@ Editor.registerPanel( 'grid-view.panel', {
     listeners: {
         'resize': '_onResize',
         'panel-show': '_onPanelShow',
+        'mousedown': '_onMouseDown',
+    },
+
+    ready: function () {
+        // this.$.pixiGrid.setScaleH( [5,2,3,2], 1, 1000, 'frame' );
+        this.$.pixiGrid.setScaleH( [5,2], 0.01, 1000 );
+        this.$.pixiGrid.setMappingH( -100, 100, 200 );
+
+        this.$.pixiGrid.setScaleV( [5,2], 0.01, 1000 );
+        this.$.pixiGrid.setMappingV( 100, -100, 200 );
     },
 
     _onResize: function ( event ) {
@@ -13,5 +23,44 @@ Editor.registerPanel( 'grid-view.panel', {
 
     _onPanelShow: function ( event ) {
         this.$.pixiGrid.autoResize();
+    },
+
+    _onMouseDown: function ( event ) {
+        if ( event.which === 1 ) {
+            event.stopPropagation();
+
+            var mousemoveHandle = function(event) {
+                event.stopPropagation();
+
+                var dx = event.clientX - this._lastClientX;
+                var dy = event.clientY - this._lastClientY;
+
+                this._lastClientX = event.clientX;
+                this._lastClientY = event.clientY;
+
+                this.$.pixiGrid.pan( dx, dy );
+                this.$.pixiGrid.repaint();
+            }.bind(this);
+
+            var mouseupHandle = function(event) {
+                event.stopPropagation();
+
+                document.removeEventListener('mousemove', mousemoveHandle);
+                document.removeEventListener('mouseup', mouseupHandle);
+
+                EditorUI.removeDragGhost();
+            }.bind(this);
+
+            //
+            this._lastClientX = event.clientX;
+            this._lastClientY = event.clientY;
+
+            //
+            EditorUI.addDragGhost("move");
+            document.addEventListener ( 'mousemove', mousemoveHandle );
+            document.addEventListener ( 'mouseup', mouseupHandle );
+
+            return;
+        }
     },
 });
