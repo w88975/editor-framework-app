d1a7cc344f37f0a401b79d242f4009927ba5de59
Johnny Wu <KarasAya@gmail.com>
2015-05-08 02:41:57 +0800

    use chokidar

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,38 @@
 var gulp = require('gulp');
+var spawn = require('child_process').spawn;
+
+var Async = require('async');
 
 // require tasks
-require('./editor-framework/tasks/download-shell');
-require('./editor-framework/tasks/build');
+require('./editor-framework/gulpfile');
+
+// var sublist = [
+//     'editor-framework',
+// ];
+// function gulpsub(task, callback) {
+//     var cmdStr = process.platform === 'win32' ? 'gulp.cmd' : 'gulp';
+
+//     function doTask (cwd, done) {
+//         console.log('Start sub-task ' + cwd);
+//         var child = spawn(cmdStr, [task], {
+//             cwd: cwd,
+//             stdio: 'inherit'
+//         });
+//         child.on('exit', function() {
+//             console.log('Finish sub-task ' + cwd);
+//             return done();
+//         });
+//     }
+
+//     Async.each( sublist, doTask, function ( err ) {
+//         console.log('Finish all sub-tasks.');
+//     });
+// }
+
+// gulp.task('build', function(done) {
+//     gulpsub('build', done);
+// });
+
+// gulp.task('build-min', function(done) {
+//     gulpsub('build-min', done);
+// });
